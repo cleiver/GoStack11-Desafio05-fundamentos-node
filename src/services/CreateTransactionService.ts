81e8b5258421a86eebf3ba7d10fd576d799b63af
cleiver <cleiver@gmail.com>
2020-04-30 15:26:31 -0300

    Updating transaction service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,20 +1,15 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
-interface RequestDTO {
+interface TransactionDTO {
   title: string;
   value: number;
   type: 'income' | 'outcome';
 }
 
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
-
-  public execute({ title, value, type }: RequestDTO): Transaction {
+  public create({ title, value, type, category }: TransactionDTO): Transaction {
     const balance = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && value > balance.total) {
